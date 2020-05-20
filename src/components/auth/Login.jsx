4eacc63f98ad92dc0912aa505ad50c910297311e
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-19 20:02:38 -0500

    armando login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 
 const Login = () => {
+  const handleData = () => {};
+
   return (
-    <div>
-      <div>Hola</div>
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar sesión</h1>
+        <form action="">
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu email..."
+              onChange={handleData}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleData}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
