e44122d8896a3cb6bde18ded37bd495a469dbbd4
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-19 21:01:12 -0500

    state para sidebar

--- src/components/proyectos/NuevoProyecto.jsx
+++ src/components/proyectos/NuevoProyecto.jsx
@@ -1,17 +1,47 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 const NuevoProyecto = () => {
+  const [proyecto, guardarProyecto] = useState({
+    nombre: "",
+  });
+
+  const { nombre } = proyecto;
+
+  //Capturando contenido del input
+  const handleAddProyect = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //envio del formulario
+  const handleSendForm = (e) => {
+    e.preventDefault();
+    //validar el proyecto
+
+    //agregar al state
+
+    //reiniciar el form
+  };
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
 
-      <form action="" className="formulario-nuevo-proyecto">
+      <form
+        action=""
+        className="formulario-nuevo-proyecto"
+        onSubmit={handleSendForm}
+      >
         <input
           type="text"
           className="input-text"
           placeholder="Nombre del proyecto"
           name="nombre"
+          value={nombre}
+          onChange={handleAddProyect}
         />
         <input
           type="submit"
